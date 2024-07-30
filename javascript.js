0f39ed10128ee7b90f80966d934898e09327c889
albert-881 <quinteroalberto88@gmail.com>
2024-07-30 14:48:41 -0500

    finished the calculator app

--- javascript.js
+++ javascript.js
@@ -1,17 +1,56 @@
+let finalResult;
+function cle(){
+    firstNum = '';
+    secondNum = '';
+    dis.innerHTML = '';
+    operator = '';
+    isOperatorClicked = false;
+
+}
 function add(number1, number2){
-    alert(number1 + number2);
+    dis.innerHTML = '';
+    finalResult = number1 + number2;
+    dis.innerHTML = `${finalResult}`
+    isOperatorClicked = false;
+    firstNum = '';
+    secondNum = '';
+    operator = '';
 }
 
 function subtract(number1, number2){
-    alert(number1 - number2);
+    dis.innerHTML = '';
+    finalResult = number1 - number2;
+    dis.innerHTML = `${finalResult}`
+    isOperatorClicked = false;
+    firstNum = '';
+    secondNum = '';
+    operator = '';
 }
 
 function multiply(number1, number2){
-    alert(number1 * number2);
+    dis.innerHTML = '';
+    finalResult = number1 * number2;
+    dis.innerHTML = `${finalResult}`
+    isOperatorClicked = false;
+    firstNum = '';
+    secondNum = '';
+    operator = '';
 }
 
 function divide(number1, number2){
-    alert(number1 / number2);
+    dis.innerHTML = '';
+    finalResult = number1 / number2;
+    if (number2 == 0){
+        alert('connot divide by 0');
+        cle();
+    }
+    else{
+    dis.innerHTML = `${finalResult}`
+    isOperatorClicked = false;
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    }
 }
 
 function operate(oper , num1, num2){
@@ -39,12 +78,66 @@ function operate(oper , num1, num2){
     }
 }
 
-let firstNum;
-let operator;
-let secondNum;
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+let result = '';
+let isOperatorClicked = false;
 
-firstNum = prompt('choose first number');
-operator = prompt('choose operator');
-secondNum = prompt('choose second number');
+//firstNum = prompt('choose first number');
+//operator = prompt('choose operator');
+//secondNum = prompt('choose second number');
 
-operate(operator,firstNum,secondNum);
+//operate(operator,firstNum,secondNum);
+
+const digits = document.querySelector('.digitButt');
+
+const dis = document.querySelector('.display');
+dis.innerHTML = '0';
+
+digits.addEventListener('click', (e) => {
+    if(!isOperatorClicked){
+        
+        firstNum += e.target.id
+        dis.innerHTML= firstNum;
+        
+    }
+    else{
+        secondNum += e.target.id
+        dis.innerHTML = `${firstNum}${operator}${secondNum}`
+    }
+    
+    
+});
+
+const oper = document.querySelector('.operator');
+
+oper.addEventListener('click', (e) => {
+    if (firstNum !== '') {
+        operator = e.target.id;
+        isOperatorClicked = true;
+        dis.innerHTML = `${firstNum}${operator}`;
+    }
+    
+})
+
+const equals = document.querySelector('.equals');
+
+equals.addEventListener('click', (e) => {
+    if (firstNum !== '' && secondNum !== '' && operator !== ''){
+    operate(operator, firstNum, secondNum);
+    }
+    else{
+        alert('you need to make sure you enter two numbers and an operator before using the equals sign');
+    }
+})
+
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click', (e) => {
+    dis.innerHTML = '';
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    isOperatorClicked = false;
+})
